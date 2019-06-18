1866e71d712b79e150e7fc3b7f0c6a6f9fb4e6e9
mostnonameuser <37878514+mostnonameuser@users.noreply.github.com>
2019-06-18 20:48:56 +0300

    Add files via upload

---
+++ h-run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+cd /home/user/TWEAK
+./tweak
+
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_HEAP_SIZE=100
+export GPU_USE_SYNC_OBJECTS=1
+
+[[ `ps aux | grep "./teamredminer" | grep -v grep | wc -l` != 0 ]] &&
+  echo -e "${RED}$MINER_NAME miner is already running${NOCOLOR}" &&
+ exit 1
+
+
+#try to release TIME_WAIT sockets
+#while true; do
+#  for con in `netstat -anp | grep TIME_WAIT | grep $MINER_API_PORT | awk '{print $5}'`; do
+#    killcx $con lo
+#  done
+#  netstat -anp | grep TIME_WAIT | grep $MINER_API_PORT &&
+#    continue ||
+#    break
+#done
+
+cd $MINER_DIR/$MINER_VER
+
+./teamredminer $(< $MINER_NAME.conf) --api_listen=0.0.0.0:${MINER_API_PORT} $@ 2>&1 | tee ${MINER_LOG_BASENAME}.log
